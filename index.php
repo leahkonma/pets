80d4d0684922f98916cc868865e9b84819ddeaff
Leah Konma <leahkonma02@gmail.com>
2024-01-18 12:20:31 -0500

    added index page

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require the autoload file
+require_once ('vendor/autoload.php');
+
+//instantiate Fat-Free
+$f3 = Base::instance(); //static method
+
+//define a default route
+$f3->route('GET /', function (){
+    echo "Pet Home";
+});
+
+//run Fat-Free
+$f3->run();
+
